51ae42ecb48f2cfb40f6e4ff2e1512bd062bbff9
ChittreshM <chittresh15512@gmail.com>
2023-11-28 13:27:35 -0800

    sucicde

--- Temp_Code_1.2.3.py
+++ Temp_Code_1.2.3.py
@@ -12,15 +12,40 @@ wn.addshape(apple_image) # Make the screen aware of the new file
 wn.addshape(pear_image)
 
 apple = trtl.Turtle()
+drawer = trtl.Turtle()
+drawer.hideturtle()
+
+#coordinates of positions
+x_pos = apple.xcor()
+y_pos = apple.ycor()
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
-  active_apple.shape(pear_image)
+def draw_fruit(active_fruit):
+  active_fruit.shape(apple_image)
   wn.update()
 
+def drop_down():
+  if apple.ycor() > -150:
+    apple.penup()
+    apple.goto(0, -150)
+
+    drawer.clear() 
+    drawer.goto(-10,-150-32)
+    drawer.write("D", font = ("Arial", 30, "normal"))
+
+def draw_letter():
+  drawer.penup()
+  drawer.hideturtle()
+  drawer.goto(-10, -30)
+  drawer.write("D", font = ("Arial", 30, "normal"))
 
 #-----function calls-----
-draw_apple(apple)
+draw_fruit(apple)
+draw_letter()
+
+wn.onkeypress(drop_down, "d")
+wn.listen()
 
 wn.mainloop()
+
