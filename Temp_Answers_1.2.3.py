e10f2758473c37b75f3dfbbebef4a8d6058219d6
ChittreshM <chittresh15512@gmail.com>
2023-12-09 01:14:27 -0800

    1.1.3 completed

---
+++ Temp_Answers_1.2.3.py
@@ -0,0 +1,50 @@
+'''1.2.3 Apple Avalanche'''			       
+
+# 1 through 3 code set-up. Done Already! 
+
+# 4 Quiz:
+# Chittresh: The active_apple parameter is a Turtle object
+
+# 5 through 8: Play with code file
+# Chittresh: done.
+
+'''Key Press'''
+# Numbers 9-13 Did you struggle with any parts of this section or have any bugs/errors in your code? If so, what were they and how did you fix/overcome them? 
+# No we did not struggle with this part of the section since this was the simplest part of the assignment
+
+'''Adding Randomness'''
+#Numbers 14-16 Did you struggle with any parts of this section or have any bugs/errors in your code? If so, what were they and how did you fix/overcome them?
+# We did struggle quite a bit when adding randomness because at first we had a very complicated process that got to the point where we didn't understand what was happening. We had to restarts this part of the code and come up with a plan before coding the rest of it. 
+
+'''Reviewing Lists'''
+# Numbers 17-20 Did you struggle with any parts of this section or have any bugs/errors in your code? If so, what were they and how did you fix/overcome them?
+# We also struggled for this section because all our previous code was only meant for one apple, so we had to change all of the previous code in order to use lists.
+
+'''Finishing the Avalanche'''
+# Numbers 21-24 Did you struggle with any parts of this section or have any bugs/errors in your code? If so, what were they and how did you fix/overcome them?
+# We did not have any struggles in the last section of this assignment 
+
+
+
+'''Conclusion'''
+# 1
+# Chittresh: I used lists in this activity to abstract away detail by holding all the different turtles and letters in different lists. This way, I could just take an element from the list every time a turtle needs to do something. It also reduces the lines of code because we didn't need to write the same code for each turtle. 
+# Aarav: In order to abstract away detail, I kept track of all the various turtles and letters in separate lists during this exercise. In this manner, any time a turtle needed to perform an action, I could simply select an item from the list. Because we didn't have to create the same code for every turtle, it also results in less lines of code. 
+
+# 2
+# Chittresh: Some of the benefits that come with using functions to abstract away detail is that the code is a lot easier to understand when reviewing it since we don't have to look through previous lines of code over and over again. Additionally, we used the same functions multiple times, which saved a lot of time in the long run. 
+# The code is much simpler to read when we review it since we don't have to keep going back over earlier lines of code, which is one advantage of utilizing functions to abstract away detail. In addition, we repeatedly employed the same functions, which ultimately resulted in significant time savings. 
+
+# 3
+# Chittresh: A programming feature that I learned from this actiivty is the onkeypress function, and I can use this feature in future programs in order to add more user interaction. 
+# Aarav: I can use the onkeypress function, a programming feature I learnt from this activity, to add additional user interaction to my future apps. 
+
+
+ 
+ 
+ 
+
+
+ 
+ 
+
--- Temp_Code_1.2.3.py
+++ Temp_Code_1.2.3.py
@@ -1,176 +1,163 @@
-######### ALL CODE GOES HERE###########
+# a123_apple_1.py
 import turtle as trtl
-import random
+import random as rand
 
-#-----setup-----
-apple_image = "apple.gif" # Store the file name of your shape
+# -----setup-----
+apple_image = "apple.gif"  # Store the file name of your shape
 pear_image = "pear.gif"
-
-picked_letter_list = []
-turtle_list = []
-letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-tree_x1 = -200
-tree_x2 = 200
-tree_y1 = 0
-tree_y2 = 125
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
-wn.bgpic("background.gif")
-wn.addshape(apple_image) # Make the screen aware of the new file
+wn.addshape(apple_image)  # Make the screen aware of the new file
 wn.addshape(pear_image)
+wn.bgpic("background.gif")
 
-fruit1 = trtl.Turtle()
-fruit1.penup()
-turtle_list.append(fruit1)
-fruit2 = trtl.Turtle()
-fruit2.penup()
-turtle_list.append(fruit2)
-fruit3 = trtl.Turtle()
-fruit3.penup()
-turtle_list.append(fruit3)
-fruit4 = trtl.Turtle()
-fruit4.penup()
-turtle_list.append(fruit4)
-fruit5 = trtl.Turtle()
-fruit5.penup()
-turtle_list.append(fruit5)
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', "L", 'M', 'N', 'O']
+appleList = []
+appleLetters = []
 
-drawer1 = trtl.Turtle()
-drawer1.hideturtle()
-drawer1.penup()
-drawer2 = trtl.Turtle()
-drawer2.hideturtle()
-drawer2.penup()
-drawer3 = trtl.Turtle()
-drawer3.hideturtle()
-drawer3.penup()
-drawer4 = trtl.Turtle()
-drawer4.hideturtle()
-drawer4.penup()
-drawer5 = trtl.Turtle()
-drawer5.hideturtle()
-drawer5.penup()
+# Create turtles for apples and assign random letters to them
+for i in range(5):
+    tempApple = trtl.Turtle()
+    appleList.append(tempApple)
+    appleLetters.append(rand.choice(letters))
 
-letter1 = ""
-letter2 = ""
-letter3 = ""
-letter4 = ""
-letter5 = ""
+# -----functions-----
+# Given a turtle, set that turtle to be shaped by the image file
+def draw_apple(index):
+    global appleLetter
+    appleList[index].penup()
+    # Use pear image if index is greater than 2, otherwise use apple image
+    if index > 2:
+        appleList[index].shape(pear_image)
+    else:
+        appleList[index].shape(apple_image)
+    wn.tracer(False)
+    # Set the position of the apple randomly
+    appleList[index].setx(rand.randint(-140, 140))
+    appleList[index].sety(rand.randint(-25, 90))
 
-#coordinates of positions
-# x_pos = apple.xcor()
-# y_pos = apple.ycor()
+    appleList[index].sety(appleList[index].ycor() - 30)
+    appleList[index].color("white")
+    # Write the corresponding letter on the apple
+    appleList[index].write(appleLetters[index], align="center", font=("Arial", 40, "bold"))
+    appleList[index].sety(appleList[index].ycor() + 30)
+    appleList[index].showturtle()
+    wn.tracer(True)
+    wn.update()
 
-#-----functions-----
-# given a turtle, set that turtle to be shaped by the image file
-def letter_pick():
-  if len(letter_list) == 0:
-    return 
-  letter1 = random.choice(letter_list)
-  print('q')
-  letter_list.remove(letter1)
-  if len(letter_list) == 0:
-    return
-  letter2 = random.choice(letter_list)
-  print('q')
-  letter_list.remove(letter2)
-  if len(letter_list) == 0:
-    return
-  letter3 = random.choice(letter_list)
-  print('q')
-  letter_list.remove(letter3)
-  if len(letter_list) == 0:
-    return
-  letter4 = random.choice(letter_list)
-  print('q')
-  letter_list.remove(letter4)
-  if len(letter_list) == 0:
-    return
-  letter5 = random.choice(letter_list)
-  print('q')
-  letter_list.remove(letter5)
-  if len(letter_list) == 0:
-    return letter1
+# Function to drop the apple (move it to the bottom and change the letter)
+def drop_apple(index):
+    appleList[index].penup()
+    appleList[index].clear()
+    appleList[index].goto(appleList[index].xcor(), -150)
+    appleList[index].hideturtle()
+    appleLetters[index] = rand.choice(letters)
+    draw_apple(index)
 
-def draw_fruits():
-  letter_pick()
-  print(letter1)
-  print(letter2)
-  print(letter3)
-  print(letter4)
-  print(letter5)
-  # letter1 = random.choice(letter_list)
-  # letter_list.remove(letter1)
-  # letter2 = random.choice(letter_list)
-  # letter_list.remove(letter2)
-  # letter3 = random.choice(letter_list)
-  # letter_list.remove(letter3)
-  # letter4 = random.choice(letter_list)
-  # letter_list.remove(letter4)
-  # letter5 = random.choice(letter_list)
-  # letter_list.remove(letter5)
-  
-  for active_turtle in turtle_list:
-    shape = random.randint(0, 1)
-    if shape == 0:
-      active_turtle.shape(apple_image)
-    elif shape == 1:
-      active_turtle.shape(pear_image)
+# Functions for each letter to check and drop corresponding apples
+def typedA():
+  for i in range(5):
+    if appleLetters[i] == 'A':
+      drop_apple(i)
+
+def typedB():
+  for i in range(5):
+      if appleLetters[i] == 'B':
+        drop_apple(i)
+
+def typedC():
+  for i in range(5):
+    if appleLetters[i] == 'C':
+      drop_apple(i)
+
+def typedD():
+  for i in range(5):
+    if appleLetters[i] == 'D':
+      drop_apple(i)
+
+def typedE():
+  for i in range(5):
+    if appleLetters[i] == 'E':
+      drop_apple(i)
+
+def typedF():
+  for i in range(5):
+    if appleLetters[i] == 'F':
+      drop_apple(i)
+
+def typedG():
+  for i in range(5):
+    if appleLetters[i] == 'G':
+      drop_apple(i)
+
+def typedH():
+  for i in range(5):
+    if appleLetters[i] == 'H':
+      drop_apple(i)
+
+def typedI():
+  for i in range(5):
+    if appleLetters[i] == 'I':
+      drop_apple(i)
+
+def typedJ():
+  for i in range(5):
+    if appleLetters[i] == 'J':
+      drop_apple(i)
     
-  fruit1.goto(random.randint(-200, -120), random.randint(50, 80))
-  fruit1pos = fruit1.pos()
-  drawer1.goto(fruit1pos[0] - 10, fruit1pos[1]- 30)
-  drawer1.write(letter1, font = ("Arial", 50, "bold"))
-  
-  fruit2.goto(random.randint(-120, -40), random.randint(tree_y1, tree_y2))
-  fruit2pos = fruit2.pos()
-  drawer2.goto(fruit2pos[0] - 10, fruit2pos[1]- 30)
-  drawer2.write(letter2, font = ("Arial", 50, "bold"))
-  
-  fruit3.goto(random.randint(-40, 40), random.randint(tree_y1, tree_y2))
-  fruit3pos = fruit3.pos()
-  drawer3.goto(fruit3pos[0] - 10, fruit3pos[1]- 30)
-  drawer3.write(letter3, font = ("Arial", 50, "bold"))
-  
-  fruit4.goto(random.randint(40, 120), random.randint(tree_y1, tree_y2))
-  fruit4pos = fruit4.pos()
-  drawer4.goto(fruit4pos[0] - 10, fruit4pos[1]- 30)
-  drawer4.write(letter4, font = ("Arial", 50, "bold"))
-  
-  fruit5.goto(random.randint(120, 200), random.randint(50, 80))
-  fruit5pos = fruit5.pos()
-  drawer5.goto(fruit5pos[0] - 10, fruit5pos[1]- 30)
-  drawer5.write(letter5, font = ("Arial", 50, "bold"))
-  
+def typedK():
+  for i in range(5):
+    if appleLetters[i] == 'K':
+      drop_apple(i)
 
-# def drop_down():
-#   if apple.ycor() > -150:
-#     apple.penup()
-#     apple.goto(0, -150)
+def typedL():
+  for i in range(5):
+    if appleLetters[i] == 'L':
+      drop_apple(i)
 
-    # drawer.clear() 
-    # drawer.goto(-10,-150-32)
-    # drawer.write("D", font = ("Arial", 30, "normal"))
+def typedM():
+  for i in range(5):
+    if appleLetters[i] == 'M':
+      drop_apple(i)
 
-# def draw_letter():
-#   drawer.penup()
-#   drawer.hideturtle()
-#   drawer.goto(-10, -30)
-#   drawer.write("D", font = ("Arial", 30, "normal"))
+def typedN():
+  for i in range(5):
+    if appleLetters[i] == 'N':
+      drop_apple(i)
 
-#-----function calls-----
-# draw_fruit(apple)
-# draw_letter()
-draw_fruits()
-draw_fruits()
-draw_fruits()
-draw_fruits()
-draw_fruits()
-draw_fruits()
-# wn.onkeypress(drop_down, "d")
+def typedO():
+  for i in range(5):
+    if appleLetters[i] == 'O':
+      drop_apple(i)
+
+# (similar functions for other letters)
+
+# -----function calls-----
+
+# Draw initial set of apples
+for i in range(5):
+    draw_apple(i)
+
+# Set up keypress listeners for each letter
+wn.onkeypress(typedA, 'A')
+wn.onkeypress(typedB, 'B')
+wn.onkeypress(typedC, 'C')
+wn.onkeypress(typedD, 'D')
+wn.onkeypress(typedE, 'E')
+wn.onkeypress(typedF, 'F')
+wn.onkeypress(typedG, 'G')
+wn.onkeypress(typedH, 'H')
+wn.onkeypress(typedI, 'I')
+wn.onkeypress(typedJ, 'J')
+wn.onkeypress(typedK, 'K')
+wn.onkeypress(typedL, 'L')
+wn.onkeypress(typedM, 'M')
+wn.onkeypress(typedN, 'N')
+wn.onkeypress(typedO, 'O')
+
 wn.listen()
 
-wn.update()
 wn.mainloop()
+
 
